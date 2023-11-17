eebb3bef9bad014ca2a4070d9d889a8c623b7754
zqleow13 <hayleyleow13@gmail.com>
2023-11-17 22:44:16 +0800

    Did authorisation and access token code
    - Requested authorisation from Spotify to access data so that Spotify will prompt user to login
    - Once user is logged in, access is authorised
    - After which, the app has to request for access token

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
+import base64
+import requests
 import random
 import string
 import urllib.parse
@@ -12,12 +14,18 @@ app = Flask(__name__)
 
 # Client ID & Redirect URI
 CLIENT_ID = os.getenv("client_ID")
-REDIRECT_URI = os.getenv("redirect_url")
+CLIENT_SECRET = os.getenv("client_SECRET")
+REDIRECT_URI = os.getenv("redirect_uri")
 
 # Generate random string for state
 def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(length))
+
+# root state
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
 # Login route
 @app.route('/login')
@@ -35,8 +43,38 @@ def login():
     })
     return redirect(spotify_auth_url)
 
-if __name__ == 'main':
-    app.run(port=9000)
+# Request access token from Spotify
+@app.route('/callback')
+def callback():
+    code = request.args.get('code', None)
+    state = request.args.get('state', None)
+
+    if state is None:
+        return redirect('/#' + urllib.parse.urlencode({'error': 'state_mismatch'}))
+    else:
+        auth_options = {
+            'url': 'https://accounts.spotify.com/api/token',
+            'data': {
+                'code': code,
+                'redirect_uri': REDIRECT_URI,
+                'grant_type': 'authorization_code'
+            },
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()
+            }
+        }
+
+        response = requests.post(auth_options['url'], data=auth_options['data'], headers=auth_options['headers'])
+        token_info = response.json()
+
+        # Handle the token information as needed (e.g., save it to session, database, etc.)
+        # For demonstration purposes, printing the token information
+        print(token_info)
+
+        return 'Token obtained successfully'
     
+    # Refresh access token
 
-
+if __name__ == 'main':
+    app.run()
